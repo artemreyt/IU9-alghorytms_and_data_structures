dac752982b932f17ba45fe09f7ab75d9646891b7
Artem Starshiv <artstarshov@list.ru>
2019-01-22 00:39:10 +0300

    my hope2

--- Csort/Csort/csort.c
+++ Csort/Csort/csort.c
@@ -7,9 +7,51 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+char    *scan_str(void);
+int     how_many_words(char *str);
+
+int main() {
+    getchar();
+    char *str = scan_str();
+    printf("%d word(s)\n", how_many_words(str));
+    puts(str);
     return 0;
+}
+
+char    *scan_str()
+{
+    char buf[1000];
+    int i = 0;
+    char c;
+    while ((c = getchar()) != '\n')
+    {
+        if (c == ' ' && (i == 0 || buf[i - 1] == ' '))
+            continue;
+        buf[i] = c;
+        i++;
+    }
+    if (buf[i - 1] == ' ')
+        i--;
+    buf[i] = '\0';
+    char *str = (char *) malloc(strlen(buf) + 1);
+    strcpy(str, buf);
+    return (str);
+}
+
+int     how_many_words(char *str)
+{
+    int spaces = 0;
+    int i = 0;
+    while(str[i])
+    {
+        if (str[i] == ' ')
+            spaces++;
+        i++;
+    }
+    if (i == 0)
+        return (0);
+    return (spaces + 1);
 }
