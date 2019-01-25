9a142806972836920b2f380a448190c5ecf6fde2
Artem Starshiv <artstarshov@list.ru>
2019-01-25 03:09:47 +0300

    Date sec

--- Csort/DateSort/DateSort/datesort.c
+++ Csort/DateSort/DateSort/datesort.c
@@ -9,6 +9,7 @@ typedef  struct Date
 void    radix_sort(date_t *arr, int n);
 void    scan_arr(date_t *arr, int n);
 void    count_sort_by_day(date_t *arr, date_t *dest, int n);
+void    count_sort_by_month(date_t *arr, date_t *dest, int n);
 
 int main()
 {
@@ -19,7 +20,7 @@ int main()
     radix_sort(arr, n);
     
     
-    free(arr);
+   // free(arr);
     return 0;
 }
 
@@ -39,25 +40,53 @@ void    radix_sort(date_t *arr, int n)
 {
     date_t *dest = (date_t *) malloc(n * sizeof(date_t));
     count_sort_by_day(arr, dest, n);
-    print_arr(dest, n);
-    //count_sort_by_month(dest, arr, n);
+    count_sort_by_month(dest, arr, n);
+    print_arr(arr, n);
     //count_sort_by_year(arr, dest, n);
-    free(arr);
-    arr = dest;
+  //  free(arr);
+ //   arr = dest;
 }
 
 void    count_sort_by_day(date_t *arr, date_t *dest, int n)
 {
-    int count[31];
+    int count[31] = {0};
     for (int i = 0; i < n; i++)
         count[arr[i].Day - 1]++;
-    for (int i = 30; i > 0; i--)
+    for (int i = 30; i >= 0; i--)
+    {
+        count[i] = 0;
         for (int j = 0; j < i; j++)
             count[i] += count[j];
+    }
     for (int i = 0; i < n; i++)
     {
         int j = count[arr[i].Day - 1];
+        
+        printf("j = %d\n", j);
+        
         dest[j] = arr[i];
         count[arr[i].Day - 1]++;
+    }
+}
+
+void    count_sort_by_month(date_t *arr, date_t *dest, int n)
+{
+    int count[12] = {0};
+    for (int i = 0; i < n; i++)
+        count[arr[i].Month - 1]++;
+    for (int i = 11; i >= 0; i--)
+    {
+        count[i] = 0;
+        for (int j = 0; j < i; j++)
+            count[i] += count[j];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int j = count[arr[i].Month - 1];
+        
+        printf("j = %d\n", j);
+        
+        dest[j] = arr[i];
+        count[arr[i].Month - 1]++;
     }
 }
