ca18f7858ac91981d9ac25e141dcd0f3cba0b23b
Artem Starshiv <artstarshov@list.ru>
2019-01-26 01:28:34 +0300

    ranges

---
+++ rangepeak.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void    scan_arr(int *arr, int n);
+int     peak(int *arr, int l, int r, int n);
+
+int     main()
+{
+    int n, m;
+    scanf("%d", &n);
+    int *arr = (int *) malloc(n * sizeof(int));
+    scan_arr(arr, n);
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++)
+    {
+        char    command[5];
+        scanf("%s", command);
+        if (strcmp(command, "PEAK") == 0)
+        {
+            int left, right;
+            scanf("%d %d", &left, &right);
+            printf("%d", peak(arr, left, right, n));
+        }
+        else
+        {
+            int index, value;
+            scanf("%d %d", &index, &value);
+            arr[index] = value;
+        }
+    }
+    free(arr);
+}
+
+void    scan_arr(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+        scanf("%d", arr + i);
+}
+
+int     peak(int *arr, int l, int r, int n)
+{
+    int count = 0;
+    for (int i = l; i <= r; i++)
+    {
+        if (i && i != n - 1 && arr[i - 1] <= arr[i] && arr[i + 1] <= arr[i])
+            count++;
+        else if (!i && arr[i + 1] <= arr[i])
+            count++;
+        else if (i == n - 1 && arr[i - 1] <= arr[i])
+            count++;
+    }
+    return (count);
+}
