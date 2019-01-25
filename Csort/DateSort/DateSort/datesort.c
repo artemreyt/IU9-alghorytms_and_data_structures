266e65e0959acace5f427c44b36ddac025ae8daa
Artem Starshiv <artstarshov@list.ru>
2019-01-25 03:40:16 +0300

    Date 4

--- Csort/DateSort/DateSort/datesort.c
+++ Csort/DateSort/DateSort/datesort.c
@@ -6,22 +6,24 @@ typedef  struct Date
     int Day, Month, Year;
 } date_t;
 
-void    radix_sort(date_t *arr, int n);
+void    radix_sort(date_t *arr, date_t *dest, int n);
 void    scan_arr(date_t *arr, int n);
 void    count_sort_by_day(date_t *arr, date_t *dest, int n);
 void    count_sort_by_month(date_t *arr, date_t *dest, int n);
 void    count_sort_by_year(date_t *arr, date_t *dest, int n);
+void    print_arr(date_t *arr, int n);
 
 int main()
 {
     int n;
     scanf("%d", &n);
     date_t *arr = (date_t *) malloc(n * sizeof(date_t));
+    date_t *dest = (date_t *) malloc(n * sizeof(date_t));
     scan_arr(arr, n);
-    radix_sort(arr, n);
-    
-    
-   free(arr);
+    radix_sort(arr, dest, n);
+    print_arr(dest, n);
+    free(arr);
+    free(dest);
     return 0;
 }
 
@@ -37,15 +39,11 @@ void    print_arr(date_t *arr, int n)
         printf("%2d %2d %d\n", arr[i].Day, arr[i].Month, arr[i].Year);
 }
 
-void    radix_sort(date_t *arr, int n)
+void    radix_sort(date_t *arr, date_t *dest, int n)
 {
-    date_t *dest = (date_t *) malloc(n * sizeof(date_t));
     count_sort_by_day(arr, dest, n);
     count_sort_by_month(dest, arr, n);
     count_sort_by_year(arr, dest, n);
-    print_arr(dest, n);;
-    free(arr);
-    arr = dest;
 }
 
 void    count_sort_by_day(date_t *arr, date_t *dest, int n)
@@ -100,9 +98,6 @@ void    count_sort_by_year(date_t *arr, date_t *dest, int n)
     for (int i = 0; i < n; i++)
     {
         int j = count[arr[i].Year - 1970];
-        
-        printf("j = %d\n", j);
-        
         dest[j] = arr[i];
         count[arr[i].Year - 1970]++;
     }
